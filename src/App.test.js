00ee2d6a06a2fa1570c4e424ecf483e3e8ac4e55
Giang-Le <giangle0411@gmail.com>
2020-08-26 20:31:53 +1000

    Init commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("render the text", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain("Testing app!");
+  ReactDOM.unmountComponentAtNode(div);
 });
